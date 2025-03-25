bc741ada3b3b647539199525c37ebdc83fd1a534
ElTrompa <anroiz@alumnatflorida.es>
2025-03-25 08:49:55 +0100

    variables y getters and setters creadas

--- UMLDGT/src/Coche.java
+++ UMLDGT/src/Coche.java
@@ -1,3 +1,22 @@
 public class Coche {
     private int numAsientos;
+    private int airbags;
+
+    public int getNumAsientos() {
+        return numAsientos;
+    }
+
+    public void setNumAsientos(int numAsientos) {
+        this.numAsientos = numAsientos;
+    }
+
+    public int getAirbags() {
+        return airbags;
+    }
+
+    public void setAirbags(int airbags) {
+        this.airbags = airbags;
+    }
+
+
 }
